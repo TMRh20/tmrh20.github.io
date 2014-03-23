a404b195d7f5637c6c40dcd6e59b85ebb15b28b6
TMRh20 <tmrh20@gmail.com>
2014-03-23 15:01:54 -0600

    Update to match current code
    see RF24 lib changes and commit history

--- RF24/search/all_c.js
+++ RF24/search/all_c.js
@@ -4,5 +4,5 @@ var searchData=
   ['testrpd',['testRPD',['../class_r_f24.html#a821285f3b54553f4402eb3fd0ac6d6c1',1,'RF24']]],
   ['todo_20list',['Todo List',['../todo.html',1,'']]],
   ['toggle_5ffeatures',['toggle_features',['../class_r_f24.html#a4a22253276f00b24dc8ab9309c03e8da',1,'RF24']]],
-  ['txstandby',['txStandBy',['../class_r_f24.html#a12cc453453c94969d4d3f0edb3778c83',1,'RF24']]]
+  ['txstandby',['txStandBy',['../class_r_f24.html#a12cc453453c94969d4d3f0edb3778c83',1,'RF24::txStandBy()'],['../class_r_f24.html#afa478a48fb80b8cca2c276c8aff32490',1,'RF24::txStandBy(bool block)']]]
 ];
--- RF24/search/functions_c.js
+++ RF24/search/functions_c.js
@@ -3,5 +3,5 @@ var searchData=
   ['testcarrier',['testCarrier',['../class_r_f24.html#ad0d522ccf39493510e64bf1740be790d',1,'RF24']]],
   ['testrpd',['testRPD',['../class_r_f24.html#a821285f3b54553f4402eb3fd0ac6d6c1',1,'RF24']]],
   ['toggle_5ffeatures',['toggle_features',['../class_r_f24.html#a4a22253276f00b24dc8ab9309c03e8da',1,'RF24']]],
-  ['txstandby',['txStandBy',['../class_r_f24.html#a12cc453453c94969d4d3f0edb3778c83',1,'RF24']]]
+  ['txstandby',['txStandBy',['../class_r_f24.html#a12cc453453c94969d4d3f0edb3778c83',1,'RF24::txStandBy()'],['../class_r_f24.html#afa478a48fb80b8cca2c276c8aff32490',1,'RF24::txStandBy(bool block)']]]
 ];
