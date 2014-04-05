0dece502c0788e3639c85f7b88a06d69ca97d62a
TMRh20 <tmrh20@gmail.com>
2014-04-05 06:30:57 -0600

    Update docs to match RF24 lib changes

--- RF24/search/functions_c.js
+++ RF24/search/functions_c.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['testcarrier',['testCarrier',['../classRF24.html#ad0d522ccf39493510e64bf1740be790d',1,'RF24']]],
   ['testrpd',['testRPD',['../classRF24.html#a821285f3b54553f4402eb3fd0ac6d6c1',1,'RF24']]],
-  ['txstandby',['txStandBy',['../classRF24.html#a12cc453453c94969d4d3f0edb3778c83',1,'RF24::txStandBy()'],['../classRF24.html#a8bc14fa0a6e1c8ad7f11eeb707841048',1,'RF24::txStandBy(unsigned long timeout)']]]
+  ['txstandby',['txStandBy',['../classRF24.html#a12cc453453c94969d4d3f0edb3778c83',1,'RF24::txStandBy()'],['../classRF24.html#aa6f36353c1bdfbaf3c530d118cb84baa',1,'RF24::txStandBy(uint32_t timeout)']]]
 ];
