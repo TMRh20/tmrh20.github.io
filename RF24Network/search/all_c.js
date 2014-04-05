6508354bc018578b10dad38afcff17341cc696d7
TMRh20 <tmrh20@gmail.com>
2014-04-05 08:41:10 -0600

    Update RF24Network docs to match code changes

--- RF24Network/search/all_c.js
+++ RF24Network/search/all_c.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['setup_5faddress',['setup_address',['../classRF24Network.html#a258cd056a41c66c201d8ee87e3f0bfc9',1,'RF24Network']]],
+  ['setup_5fwatchdog',['setup_watchdog',['../classRF24Network.html#acb09129760ac9b171833af3055b2b6f5',1,'RF24Network']]],
   ['sleepnode',['sleepNode',['../classRF24Network.html#a8921392fbd695721c3d137f139a2a56e',1,'RF24Network']]],
   ['spi',['SPI',['../RF24Network__config_8h.html#a2332dcef61deaf30b01014f3bede4343',1,'RF24Network_config.h']]],
   ['strlen_5fp',['strlen_P',['../RF24Network__config_8h.html#a05dde754db9944c90362c1f861d358ba',1,'RF24Network_config.h']]],
--- RF24Network/search/functions_9.js
+++ RF24Network/search/functions_9.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['setup_5faddress',['setup_address',['../classRF24Network.html#a258cd056a41c66c201d8ee87e3f0bfc9',1,'RF24Network']]],
+  ['setup_5fwatchdog',['setup_watchdog',['../classRF24Network.html#acb09129760ac9b171833af3055b2b6f5',1,'RF24Network']]],
   ['sleepnode',['sleepNode',['../classRF24Network.html#a8921392fbd695721c3d137f139a2a56e',1,'RF24Network']]],
   ['sync',['Sync',['../classSync.html#a25ef5713f388ddc4dc294e1ce8776872',1,'Sync']]]
 ];
