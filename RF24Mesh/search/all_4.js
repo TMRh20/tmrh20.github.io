9c34deef8ae541d061c6e38ece228aec70d65e4b
TMRh20 <tmrh20@gmail.com>
2014-10-05 22:33:49 -0600

    RF24Mesh: Examples Update

--- RF24Mesh/search/all_4.js
+++ RF24Mesh/search/all_4.js
@@ -4,8 +4,6 @@ var searchData=
   ['main',['main',['../RF24Mesh__Example_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;RF24Mesh_Example.cpp'],['../RF24Mesh__Example__Master_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627',1,'main(int argc, char **argv):&#160;RF24Mesh_Example_Master.cpp']]],
   ['mesh',['mesh',['../RF24Mesh__Example_8cpp.html#a97c1eaa120a7add40ebbfb9b3d9268a4',1,'mesh():&#160;RF24Mesh_Example.cpp'],['../RF24Mesh__Example__Master_8cpp.html#a97c1eaa120a7add40ebbfb9b3d9268a4',1,'mesh():&#160;RF24Mesh_Example_Master.cpp']]],
   ['mesh_5faddress',['mesh_address',['../classRF24Mesh.html#abc64907a8d011fe4f8741a0be8560db4',1,'RF24Mesh']]],
-  ['mesh_5fdebug',['MESH_DEBUG',['../RF24Mesh__config_8h.html#a194480fecbb74eca31aa4fa266345294',1,'RF24Mesh_config.h']]],
-  ['mesh_5fdebug_5fserial',['MESH_DEBUG_SERIAL',['../RF24Mesh__config_8h.html#a6e706a68ed6a224ff593cca98152fc09',1,'RF24Mesh_config.h']]],
   ['mesh_5fdefault_5faddress',['MESH_DEFAULT_ADDRESS',['../RF24Mesh__config_8h.html#a9aa307506ecde1e60194a4eed4e073c9',1,'RF24Mesh_config.h']]],
   ['mesh_5fdefault_5fchannel',['MESH_DEFAULT_CHANNEL',['../RF24Mesh__config_8h.html#a8a8ab350f590431aad2791ed1cf36177',1,'RF24Mesh_config.h']]],
   ['mesh_5fmax_5fchildren',['MESH_MAX_CHILDREN',['../RF24Mesh__config_8h.html#a34a829ec67e8c11ed0416fe1ee5e3bae',1,'RF24Mesh_config.h']]],
--- RF24Mesh/search/defines_0.js
+++ RF24Mesh/search/defines_0.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['mesh_5fdebug',['MESH_DEBUG',['../RF24Mesh__config_8h.html#a194480fecbb74eca31aa4fa266345294',1,'RF24Mesh_config.h']]],
-  ['mesh_5fdebug_5fserial',['MESH_DEBUG_SERIAL',['../RF24Mesh__config_8h.html#a6e706a68ed6a224ff593cca98152fc09',1,'RF24Mesh_config.h']]],
   ['mesh_5fdefault_5faddress',['MESH_DEFAULT_ADDRESS',['../RF24Mesh__config_8h.html#a9aa307506ecde1e60194a4eed4e073c9',1,'RF24Mesh_config.h']]],
   ['mesh_5fdefault_5fchannel',['MESH_DEFAULT_CHANNEL',['../RF24Mesh__config_8h.html#a8a8ab350f590431aad2791ed1cf36177',1,'RF24Mesh_config.h']]],
   ['mesh_5fmax_5fchildren',['MESH_MAX_CHILDREN',['../RF24Mesh__config_8h.html#a34a829ec67e8c11ed0416fe1ee5e3bae',1,'RF24Mesh_config.h']]],
