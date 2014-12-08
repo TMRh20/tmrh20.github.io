fdf8326a29971330bf5968e4c08f942ac36d26cc
TMRh20 <tmrh20@gmail.com>
2014-12-08 00:11:30 -0600

    RF24Ethernet Docs update
    - udpate for new examples etc

--- RF24Ethernet/search/variables_5.js
+++ RF24Ethernet/search/variables_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['mydata',['myData',['../classRF24EthernetClass.html#a180a7595aa35a960e8df1e3f637e1545',1,'RF24EthernetClass']]],
-  ['mydataout',['myDataOut',['../structuip__userdata__t.html#a76257a3cfcd4251d9a6962747b922e4f',1,'uip_userdata_t']]]
+  ['mydataout',['myDataOut',['../structuip__userdata__t.html#a82332308d3217b4d0c1fc6e75ed0b072',1,'uip_userdata_t']]]
 ];
