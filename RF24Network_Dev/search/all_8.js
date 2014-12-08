fd7e0f4be18355fc97cfe4b806eb457ac47a3472
TMRh20 <tmrh20@gmail.com>
2014-12-08 06:27:36 -0600

    RF24Network DEV Docs Update
    - Update for recent changes and RPi/Arduino code merge

--- RF24Network_Dev/search/all_8.js
+++ RF24Network_Dev/search/all_8.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['parent',['parent',['../classRF24Network.html#a2274c0548e35b5182a2f6ced222d0699',1,'RF24Network']]],
   ['peek',['peek',['../classRF24Network.html#a04fff614f10fcc1d8984b5ee5cc87a4b',1,'RF24Network']]],
-  ['peekdata',['peekData',['../classRF24Network.html#a821a40cd3c7afdff86742de2816b67fc',1,'RF24Network']]],
   ['performance_20and_20data_20loss_3a_20tuning_20the_20network',['Performance and Data Loss: Tuning the Network',['../Tuning.html',1,'']]]
 ];
--- RF24Network_Dev/search/functions_5.js
+++ RF24Network_Dev/search/functions_5.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['parent',['parent',['../classRF24Network.html#a2274c0548e35b5182a2f6ced222d0699',1,'RF24Network']]],
-  ['peek',['peek',['../classRF24Network.html#a04fff614f10fcc1d8984b5ee5cc87a4b',1,'RF24Network']]],
-  ['peekdata',['peekData',['../classRF24Network.html#a821a40cd3c7afdff86742de2816b67fc',1,'RF24Network']]]
+  ['peek',['peek',['../classRF24Network.html#a04fff614f10fcc1d8984b5ee5cc87a4b',1,'RF24Network']]]
 ];
