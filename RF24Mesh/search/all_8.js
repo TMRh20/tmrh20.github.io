cf626a82ed13921de101be08bce85bc0f1d6918d
TMRh20 <tmrh20@gmail.com>
2015-06-13 10:48:22 -0600

    Update docs for RF24Mesh changes

--- RF24Mesh/search/all_8.js
+++ RF24Mesh/search/all_8.js
@@ -9,6 +9,7 @@ var searchData=
   ['mesh_5faddress',['mesh_address',['../classRF24Mesh.html#abc64907a8d011fe4f8741a0be8560db4',1,'RF24Mesh']]],
   ['mesh_5fdefault_5faddress',['MESH_DEFAULT_ADDRESS',['../RF24Mesh__config_8h.html#a9aa307506ecde1e60194a4eed4e073c9',1,'RF24Mesh_config.h']]],
   ['mesh_5fdefault_5fchannel',['MESH_DEFAULT_CHANNEL',['../RF24Mesh__config_8h.html#a8a8ab350f590431aad2791ed1cf36177',1,'RF24Mesh_config.h']]],
+  ['mesh_5fid_5flookup',['MESH_ID_LOOKUP',['../RF24Mesh_8h.html#a3a8a9cb5ab86651611a15fa52742ccc7',1,'RF24Mesh.h']]],
   ['mesh_5fmax_5faddresses',['MESH_MAX_ADDRESSES',['../RF24Mesh__config_8h.html#a27d0870e4b14cc018a97e1c52cd23d77',1,'RF24Mesh_config.h']]],
   ['mesh_5fmax_5fchildren',['MESH_MAX_CHILDREN',['../RF24Mesh__config_8h.html#a34a829ec67e8c11ed0416fe1ee5e3bae',1,'RF24Mesh_config.h']]],
   ['mesh_5fmin_5fsave_5ftime',['MESH_MIN_SAVE_TIME',['../RF24Mesh__config_8h.html#adc9609391ebbc451c866fc20c42570b3',1,'RF24Mesh_config.h']]],
--- RF24Mesh/search/functions_3.js
+++ RF24Mesh/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['getaddress',['getAddress',['../classRF24Mesh.html#aeba97211b257a8cc57ea0fc242f9135e',1,'RF24Mesh']]],
-  ['getnodeid',['getNodeID',['../classRF24Mesh.html#ab567773e39c2baacb373e5bc104bc6e4',1,'RF24Mesh::getNodeID()'],['../classRF24Mesh.html#ae2553e231cd3083ee6cd533303854b6e',1,'RF24Mesh::getNodeId(uint16_t address)']]]
+  ['getnodeid',['getNodeID',['../classRF24Mesh.html#a5a306319f0722c77ee4e884b38d916af',1,'RF24Mesh']]]
 ];
