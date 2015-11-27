bff933c829d66d8acc0e4b73a5825444569ac789
TMRh20 <tmrh20@gmail.com>
2015-11-27 10:44:41 -0600

    RF24Mesh Docs - Update for v1.0.3

--- RF24Mesh/search/all_c.js
+++ RF24Mesh/search/all_c.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['savedhcp',['saveDHCP',['../classRF24Mesh.html#aa9448ca15e501d0bae687c5428c7cc12',1,'RF24Mesh']]],
-  ['send',['send',['../classRF24Mesh.html#ab413db470b2a27c9c2088c77e564b010',1,'RF24Mesh']]],
-  ['sendp',['sendP',['../classRF24Mesh.html#a306869b1975a77d5483bcd7fc5b827c5',1,'RF24Mesh']]],
+  ['setaddress',['setAddress',['../classRF24Mesh.html#a6aafd9dbdd198487de392f9992218f04',1,'RF24Mesh']]],
   ['setchannel',['setChannel',['../classRF24Mesh.html#ab4d6a0a2146215b0e54dff4e1dcb08d1',1,'RF24Mesh']]],
+  ['setchild',['setChild',['../classRF24Mesh.html#ae183c358915a1b7fbf1b19db8ba66a16',1,'RF24Mesh']]],
   ['setnodeid',['setNodeID',['../classRF24Mesh.html#a9d3e662216ffd890e019537a95297ed4',1,'RF24Mesh']]],
   ['setstaticaddress',['setStaticAddress',['../classRF24Mesh.html#a2032d5e5feabe69ecd89f937e0efafed',1,'RF24Mesh']]],
   ['setup_20and_20config',['Setup And Config',['../Setup-Config.html',1,'']]]
--- RF24Mesh/search/all_e.js
+++ RF24Mesh/search/all_e.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['write',['write',['../classRF24Mesh.html#a34535daf39ead14698f3718037358394',1,'RF24Mesh::write(const void *data, uint8_t msg_type, size_t size, uint8_t nodeID=0)'],['../classRF24Mesh.html#ad294234c41f303881aa24c74782eecd7',1,'RF24Mesh::write(uint16_t to_node, const void *data, uint8_t msg_type, size_t size)']]],
-  ['writep',['writeP',['../classRF24Mesh.html#a0638c31ee67dc9bbf266dd505627e030',1,'RF24Mesh']]]
+  ['write',['write',['../classRF24Mesh.html#a34535daf39ead14698f3718037358394',1,'RF24Mesh::write(const void *data, uint8_t msg_type, size_t size, uint8_t nodeID=0)'],['../classRF24Mesh.html#ad294234c41f303881aa24c74782eecd7',1,'RF24Mesh::write(uint16_t to_node, const void *data, uint8_t msg_type, size_t size)']]]
 ];
