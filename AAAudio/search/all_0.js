6626ff28482c7ab5a22005302b21758cc08e641e
TMRh20 <tmrh20@gmail.com>
2016-11-09 20:52:45 -0600

    AAAudio: Update docs

--- AAAudio/search/all_0.js
+++ AAAudio/search/all_0.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['adcbuffer',['adcBuffer',['../classAutoAnalog.html#a257d95cf5a0b43deaef32b761fc25792',1,'AutoAnalog']]],
+  ['autoadjust',['autoAdjust',['../classAutoAnalog.html#a34814ebff21d9b602f1f33503c070a54',1,'AutoAnalog']]],
   ['autoanalog',['AutoAnalog',['../classAutoAnalog.html',1,'AutoAnalog'],['../classAutoAnalog.html#a3667d5820bc647c87517c1eaea8ff722',1,'AutoAnalog::AutoAnalog()']]],
   ['autoanalog_5fconfig_2eh',['AutoAnalog_config.h',['../AutoAnalog__config_8h.html',1,'']]],
   ['autoanalogaudio_2ecpp',['AutoAnalogAudio.cpp',['../AutoAnalogAudio_8cpp.html',1,'']]],
--- AAAudio/search/variables_0.js
+++ AAAudio/search/variables_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['adcbuffer',['adcBuffer',['../classAutoAnalog.html#a257d95cf5a0b43deaef32b761fc25792',1,'AutoAnalog']]]
+  ['adcbuffer',['adcBuffer',['../classAutoAnalog.html#a257d95cf5a0b43deaef32b761fc25792',1,'AutoAnalog']]],
+  ['autoadjust',['autoAdjust',['../classAutoAnalog.html#a34814ebff21d9b602f1f33503c070a54',1,'AutoAnalog']]]
 ];
