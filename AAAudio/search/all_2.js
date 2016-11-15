b5f061d94c8b0a6591cb5b49695dc9d79a7d77bd
TMRh20 <tmrh20@gmail.com>
2016-11-14 19:55:43 -0600

    AAAudio Docs Update

--- AAAudio/search/all_2.js
+++ AAAudio/search/all_2.js
@@ -5,5 +5,6 @@ var searchData=
   ['dacbuffer16',['dacBuffer16',['../classAutoAnalog.html#a85377e5dc495fdbd129db30c6c88ca71',1,'AutoAnalog']]],
   ['dachandler',['dacHandler',['../classAutoAnalog.html#a0bf0cb2ddbf79748887551f09a7932ec',1,'AutoAnalog']]],
   ['default_5ffrequency',['DEFAULT_FREQUENCY',['../AutoAnalog__config_8h.html#a052b63090cda80236391c5fc0c1fe30c',1,'AutoAnalog_config.h']]],
-  ['disableadcchannel',['disableAdcChannel',['../classAutoAnalog.html#af23ab64cf3d4881f55ba1d5c0b1d607b',1,'AutoAnalog']]]
+  ['disableadcchannel',['disableAdcChannel',['../classAutoAnalog.html#af23ab64cf3d4881f55ba1d5c0b1d607b',1,'AutoAnalog']]],
+  ['disabledac',['disableDAC',['../classAutoAnalog.html#a7529756d22a1082283fa86994b87a896',1,'AutoAnalog']]]
 ];
