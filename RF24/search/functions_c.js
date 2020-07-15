7dee1e03f28d919c838801de58b4ce0b698df4d6
TMRh20 <tmrh20@gmail.com>
2020-07-14 20:36:18 -0600

    Update RF24 docs
    - Remove RPi/BCM specific information that is no longer required
    - Display info for unified constructor that works between all platforms

--- RF24/search/functions_c.js
+++ RF24/search/functions_c.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['read',['read',['../classRF24.html#a8e2eacacfba96426c192066f04054c5b',1,'RF24::read()'],['../group__Porting__GPIO.html#ga783c8d81b91f178759766e7e62ea4b17',1,'read():&#160;gpio.h']]],
   ['reusetx',['reUseTX',['../classRF24.html#aeaf7fa54d3ab2a85ce215b4bf6ae933b',1,'RF24']]],
-  ['rf24',['RF24',['../classRF24.html#af564e19261afb56005d2411d0c7b05de',1,'RF24::RF24(uint16_t _cepin, uint16_t _cspin)'],['../classRF24.html#a085d99c986effa19b01c030e2b1439ce',1,'RF24::RF24(uint16_t _cepin, uint16_t _cspin, uint32_t spispeed)']]],
+  ['rf24',['RF24',['../classRF24.html#abe8f878e238644252ccf5ec27fba611d',1,'RF24']]],
   ['rxfifofull',['rxFifoFull',['../classRF24.html#ad22e44fe1a68747872fcb304a407fd30',1,'RF24']]]
 ];
--- RF24/search/pages_7.js
+++ RF24/search/pages_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['rf24_20portability',['RF24 Portability',['../Portability.html',1,'']]],
-  ['raspberry_20pi',['Raspberry Pi',['../RPi.html',1,'']]]
+  ['rf24_20portability',['RF24 Portability',['../Portability.html',1,'']]]
 ];
