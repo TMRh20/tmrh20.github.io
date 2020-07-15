4f88c52d19af708c7120fa4fc8b0ad558dcf7959
TMRh20 <tmrh20@gmail.com>
2020-07-14 21:00:06 -0600

    Try that again
    Delete old files before regeneration

--- RF24/search/typedefs_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['prog_5fuint16_5ft',['prog_uint16_t',['../RF24__config_8h.html#a05ca2f20b55933ad782917161d54287d',1,'prog_uint16_t():&#160;RF24_config.h'],['../group__Porting__General.html#ga05ca2f20b55933ad782917161d54287d',1,'prog_uint16_t():&#160;RF24_arch_config.h']]]
-];
