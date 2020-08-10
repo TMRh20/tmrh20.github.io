ac1aa05928d8eaf14ff63e4331f99d7d1cd2eb9a
TMRh20 <tmrh20@gmail.com>
2020-08-10 11:43:16 -0600

    Update RF24 Docs
    - Add datasheets
    - Updates to main page, arduino, linux, linux/rpi & mraa sections
    - Update info for openReadingPipe

--- RF24/search/pages_3.js
+++ RF24/search/pages_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['linux_20cross_2dcompilation_449',['Linux cross-compilation',['../CrossCompile.html',1,'']]],
-  ['linux_20devices_450',['Linux devices',['../Linux.html',1,'']]],
+  ['linux_20installation_450',['Linux Installation',['../Linux.html',1,'']]],
   ['linux_20general_2fraspberry_20pi_451',['Linux General/Raspberry Pi',['../RPi.html',1,'']]]
 ];
